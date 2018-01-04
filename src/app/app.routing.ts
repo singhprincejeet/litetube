b55eca12ed3839a70ee12970396fa3c78b716395
Princejeet Singh Sandhu <singhprincejeet@gmail.com>
2018-01-04 01:59:58 -0500

    Implemented video page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from 'app/home/home.component';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { SearchResultsComponent } from 'app/search-results/search-results.component';
+import { VideoComponent } from 'app/video/video.component';
 
 const AppRouter: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'results/:query', component: SearchResultsComponent}
+    {path: 'results/:query', component: SearchResultsComponent},
+    {path: 'video/:videoId', component: VideoComponent},
 ]
 
 export const AppRoutes: ModuleWithProviders = RouterModule.forRoot(AppRouter);
